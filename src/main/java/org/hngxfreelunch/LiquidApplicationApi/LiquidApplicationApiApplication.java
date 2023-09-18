e2d7a4186bc1218a503b83f72eec6d325f8d5b95
oresho <somuyiwaoreoluwa@gmail.com>
2023-09-18 21:56:55 +0100

    Initial commit

---
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/LiquidApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package org.hngxfreelunch.LiquidApplicationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hngxfreelunch/LiquidApplicationApi/LiquidApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hngxfreelunch.LiquidApplicationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
