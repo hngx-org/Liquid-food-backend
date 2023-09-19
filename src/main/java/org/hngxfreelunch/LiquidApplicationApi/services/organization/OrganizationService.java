6639326616d66f28cb49f8d9ca0c77fa13c8bed7
gitwithlucky <luckyikenkwocha@gmail.com>
2023-09-19 21:29:32 +0100

    Dev

--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/organization/OrganizationService.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/organization/OrganizationService.java
@@ -3,6 +3,7 @@ package org.hngxfreelunch.LiquidApplicationApi.services.organization;
 
 public interface OrganizationService {
     // TODO: CREATE ORGANIZATION
+
     // TODO: ADD STAFF
     // TODO: REMOVE STAFF
     // TODO: UPDATE ORGANIZATION DETAILS
