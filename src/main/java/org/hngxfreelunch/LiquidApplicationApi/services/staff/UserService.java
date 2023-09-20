29ced34b852234b727e9f4dec56b4701c1ba86d7
Igbokwe Victor <igbokwevictor122@gmail.com>
2023-09-20 17:23:00 +0100

    changes

--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/staff/UserService.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/staff/UserService.java
@@ -1,10 +1,15 @@
 package org.hngxfreelunch.LiquidApplicationApi.services.staff;
 
 
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.payload.BankRequestDto;
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.response.ApiResponseDto;
+
 public interface UserService {
     // TODO: CREATE STAFF
     // TODO: GET STAFF DETAILS BY NAME OR EMAIL
-    // TODO: UPDATE STAFF DETAILS
     // TODO: ADD BANK DETAILS
+    public ApiResponseDto addBankDetails(BankRequestDto bankRequestDto);
+
     // TODO: GET ALL USERS
+    public ApiResponseDto getAllUsers();
 }
--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/staff/UserServiceImplementation.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/staff/UserServiceImplementation.java
@@ -1,8 +1,16 @@
 package org.hngxfreelunch.LiquidApplicationApi.services.staff;
 
 import lombok.RequiredArgsConstructor;
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.UserDto;
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.payload.BankRequestDto;
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.response.ApiResponseDto;
+import org.hngxfreelunch.LiquidApplicationApi.data.entities.User;
 import org.hngxfreelunch.LiquidApplicationApi.data.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +18,23 @@ public class UserServiceImplementation implements UserService {
 
     private UserRepository userRepository;
 
+    @Override
+    public ApiResponseDto addBankDetails(BankRequestDto bankRequestDto) {
+
+        User user = new User();
+        user.setBank_number(bankRequestDto.getBankNumber());
+        user.setBank_code(bankRequestDto.getBankCode());
+        user.setBank_name(bankRequestDto.getBankName());
+
+        userRepository.save(user);
+        return new ApiResponseDto(user, "successfully created bank account", 200);
+    }
+
+    @Override
+    public ApiResponseDto getAllUsers() {
+
+        List<User> users = userRepository.findAll();
+
+        return new ApiResponseDto(users, "all users", 200);
+    }
 }
