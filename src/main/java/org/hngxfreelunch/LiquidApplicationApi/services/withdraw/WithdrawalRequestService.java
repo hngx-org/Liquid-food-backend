4d377a69a50d7da7247e8dbeaaf18132a7c0e508
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-09-20 10:35:47 +0100

    add withdrawal service

--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/withdraw/WithdrawalRequestService.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/withdraw/WithdrawalRequestService.java
@@ -1,6 +1,12 @@
 package org.hngxfreelunch.LiquidApplicationApi.services.withdraw;
 
 
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.WithdrawalResponse;
+import org.hngxfreelunch.LiquidApplicationApi.data.entities.WithdrawalRequest;
+
 public interface WithdrawalRequestService {
+
+    WithdrawalResponse addWithdrawalRequest(Long staffId, Integer creditsToWithdraw);
     // TODO: ADD NEW WITHDRAWAL REQUEST
+
 }
--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/withdraw/WithdrawalRequestServiceImplementation.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/services/withdraw/WithdrawalRequestServiceImplementation.java
@@ -1,6 +1,11 @@
 package org.hngxfreelunch.LiquidApplicationApi.services.withdraw;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.hngxfreelunch.LiquidApplicationApi.data.dtos.WithdrawalResponse;
+import org.hngxfreelunch.LiquidApplicationApi.data.entities.Staff;
+import org.hngxfreelunch.LiquidApplicationApi.data.entities.WithdrawalRequest;
+import org.hngxfreelunch.LiquidApplicationApi.data.repositories.StaffRepository;
 import org.hngxfreelunch.LiquidApplicationApi.data.repositories.WithdrawalRequestRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +13,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class WithdrawalRequestServiceImplementation implements WithdrawalRequestService {
 
-    private WithdrawalRequestRepository withdrawalRequestRepository;
+    private final WithdrawalRequestRepository withdrawalRequestRepository;
+
+    private final StaffRepository staffRepository;
+
+    @Override
+    public WithdrawalResponse addWithdrawalRequest(Long staffId, Integer creditsToWithdraw) {
+        // Find the staff member by ID
+        Staff staff = staffRepository.findById(staffId)
+                .orElseThrow(() -> new EntityNotFoundException("Staff member not found"));
+
+        // Create a new WithdrawalRequest
+        WithdrawalRequest withdrawalRequest = new WithdrawalRequest();
+        withdrawalRequest.setStaff(staff);
+        withdrawalRequest.setLunchCredits(creditsToWithdraw);
+
+        // Save the withdrawal request
+        withdrawalRequestRepository.save(withdrawalRequest);
+
+        // Return a response
+        return WithdrawalResponse
+                .builder()
+                .message("Withdrawal request created successfully with ID:" + withdrawalRequest.getId())
+                .build();
+    }
 }
