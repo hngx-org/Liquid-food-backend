2a7050ec0d87164f0b01d1c2172b682d4bab6a39
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-09-21 12:28:34 +0100

    Unit test on the UserController

--- src/test/java/org/hngxfreelunch/LiquidApplicationApi/LiquidApplicationApiApplicationTests.java
+++ src/test/java/org/hngxfreelunch/LiquidApplicationApi/LiquidApplicationApiApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.hngxfreelunch.LiquidApplicationApi;
 
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,3 @@ class LiquidApplicationApiApplicationTests {
 	}
 
 }
-=======
-package org.hngxfreelunch.LiquidApplicationApi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LiquidApplicationApiApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
-
-}
->>>>>>> 37617686d864be3d5717d47a2fb029fe19dd3361
