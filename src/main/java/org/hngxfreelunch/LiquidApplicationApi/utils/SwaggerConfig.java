2f67bb5918ff58064772b4dd2ffcc9dd95e99e19
Mhister-Jhay <omosighojosh@gmail.com>
2023-09-23 14:35:11 +0100

    Application properties

--- src/main/java/org/hngxfreelunch/LiquidApplicationApi/utils/SwaggerConfig.java
+++ src/main/java/org/hngxfreelunch/LiquidApplicationApi/utils/SwaggerConfig.java
@@ -1,18 +1,30 @@
 package org.hngxfreelunch.LiquidApplicationApi.utils;
 
-import io.swagger.annotations.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI api() {
+        SecurityScheme securityScheme  = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearer-auth",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer")));
+                .components(new Components()
+                        .addSecuritySchemes("jwt",securityScheme))
+                .info(new Info()
+                        .title("FREE LUNCH - TEAM LIQUID")
+                        .version("v1"))
+                .security(Collections.singletonList(new SecurityRequirement().addList("jwt")));
     }
 }
